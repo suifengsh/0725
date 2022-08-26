c93c42b4a16b8897838399050a7b1c79dc029096
小菜园 <27824544@qq.com>
2022-08-26 09:46:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
-echo 'src-git opentopd https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+echo 'src-git opentopd https://github.com/suifengsh/sirpdboy-package' >>feeds.conf.default
